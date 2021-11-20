bafc77d80ef84e5cb3c7a75a7b2b030f1ef63eb6
olijeffers0n <69084614+olijeffers0n@users.noreply.github.com>
2021-11-20 13:28:46 +0000

    Initial Commit

---
+++ src/main/java/xyz/oli/launcher.java
@@ -0,0 +1,11 @@
+package xyz.oli;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class launcher extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Hi");
+    }
+}
